11b5471649bbb46e7e47e4299323cbf49def90a2
Quinn Cassady <600020067@fjuhsd.org>
2021-02-12 16:53:58 +0000

    i did the lab

--- Number.java
+++ Number.java
@@ -1,7 +1,12 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Quinn Cassady
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
 
 //write the Number class
 //use the handout and
@@ -10,19 +15,63 @@
 
 //header for Number class
 
-//private instance variable (must be an Integer)
+public class Number
+{
 
-//constructor
+  //private instance variable (must be an Integer)
 
+  private Integer num;
 
-//getter method
+  //constructor
 
+  public Number(){
+    num = 0;
+  }
+  public Number(Integer n){
+    num = n;
+  }
 
-//setter method
+  //getter method
 
-//isOdd method
+  public Integer getNum(){
+    return num;
+  }
 
-//isPerfect method
+  //setter method
 
+  public void setNum(Integer newNum){
+    num = newNum;
+  }
 
-//toString
+  //isOdd method
+
+  public boolean isOdd(){
+    //return the result of whether the remainder or num/2 is 1 or not
+    return (num % 2 == 1);
+  }
+
+  //isPerfect method
+
+  public boolean isPerfect(){
+    //create an int 
+    int sum = 0;
+    //make a for loop with i=1, i<num, and i increase by 1 each time
+    for(int i = 1; i < num; i++){
+      //if i is a factor of num,
+      if(num % i == 0){
+        //then add i to the sum
+        sum += i;
+      //end the if
+      }
+    //end the for
+    }
+    //return if the sum is equal to num
+    return (sum == num);
+  }
+
+  //toString
+
+  public String toString(){
+    return "Number: " + num;
+  }
+}
